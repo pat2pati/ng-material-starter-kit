aa214f6472fbafdaee52ead902c078c293c5da6f
Patrycja <czerwionkapatrycja@gmail.com>
2022-11-23 11:32:51 +0100

    feat: employee form implemented

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,6 @@
+export interface EmployeeModel {
+  readonly name: string;
+  readonly salary: string;
+  readonly age: number;
+  readonly id: number;
+}
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EmployeeModel } from '../models/employee.model';
+
+@Injectable()
+export class EmployeeService {
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  create(employee: Omit<EmployeeModel, 'id'>): Observable<EmployeeModel> {
+    return this._httpClient.post<EmployeeModel>('https://dummy.restapiexample.com/create', employee);
+  }
+}
